4813f6e7d94944a11e2495790e7543da284b55c0
DiFGit <55925077+DiFGit@users.noreply.github.com>
2020-04-13 23:09:50 +0100

    trying to fix deployment

---
+++ build/precache-manifest.e7d2020f06a6c5b59d0f2359cb64dcd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55fd1c14de6b493e693b2408f77b4633",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5825b3490dab8405af15",
+    "url": "/static/css/main.f2cc1a29.chunk.css"
+  },
+  {
+    "revision": "cb2aa694feae9dbbd508",
+    "url": "/static/js/2.97bb058a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.97bb058a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5825b3490dab8405af15",
+    "url": "/static/js/main.32d94233.chunk.js"
+  },
+  {
+    "revision": "ca9a3fbc7a7d3544e958",
+    "url": "/static/js/runtime-main.628354ec.js"
+  }
+]);
